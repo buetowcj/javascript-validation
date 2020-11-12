457337a8234076395dadffe65526d0906b51c80a
buetowcj <cbuetow@my.wctc.edu>
2020-11-12 14:25:04 -0600

    Initial commit

--- validation.js
+++ validation.js
@@ -1,23 +1,51 @@
 $(document).ready(function () {
     // Object containing the validation rules
     var myRules =
-        {};
+        {
+            firstName:
+                {
+                    required: true
+                },
+            grade:
+                {
+                    min: 5,
+                    max: 8,
+                    digits: true
+                }
+        };
 
     // Object containing the error messages
     var myMessages =
-        {};
+        {
+            firstName:
+                {
+                    required: "Field required"
+                },
+            grade:
+                {
+                    min: "Too small",
+                    max: "Too big",
+                    digits: "Bad"
+                }
+        };
 
     // Pass the configuration to the form's validate() method
     // Needs submitHandler, rules, and messages properties
     $("form").validate(
-        {}
+        {
+            submitHandler: runMyProgram,
+            rules: myRules,
+            messages: myMessages
+        }
     );
 
     function runMyProgram() {
         // Change the text of the <p> with ID of "message" to
         // state, for example, "You have registered Jane for grade 6 camp!"
         // Use the name and grade the user provided in the form.
-
+        var firstName = $("#firstName").val();
+        var gradeCount = parseInt($("#grade").val());
+        $("p").text(`You have registered ${firstName} for grade ${gradeCount} camp!`)
 
     }
 
